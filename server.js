2e3925cc889cd7eb0762e30f09c4809c437bda21
djrhoten <djrhoten@gmail.com>
2021-07-22 01:23:34 -0500

    complete-ish

---
+++ server.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+// to seed the database with some example data if desired
+// const seedAll = require('./seeds/index-seeds');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//cookie session
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    //time until session is destroyed = 5 minutes, 
+    //which was for testing, but can be changed here
+    maxAge: 60 * 1000 * 5
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+//express using database, cookies
+app.use(session(sess));
+
+//helper functions
+const helpers = require('./utils/helpers');
+
+//handlebars using helper functions
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+//database and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
